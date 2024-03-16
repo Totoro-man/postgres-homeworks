82d740d042bb3e7b025ef7177291effea0c658ba
Тоторо <roman.aka.tod@gmail.com>
2024-03-16 04:15:09 +0300

    + Контроша 5
    без последнего задания

--- homework-5/main.py
+++ homework-5/main.py
@@ -4,11 +4,13 @@ import psycopg2
 
 from config import config
 
+from utils.utils import *
+
 
 def main():
     script_file = 'fill_db.sql'
     json_file = 'suppliers.json'
-    db_name = 'my_new_db'
+    db_name = 'hw5'
 
     params = config()
     conn = None
@@ -42,26 +44,51 @@ def main():
 
 def create_database(params, db_name) -> None:
     """Создает новую базу данных."""
-    pass
+    bu = BaseUtils('admin', db_name)
+    bu.init_base()
+    del bu
+
 
 def execute_sql_script(cur, script_file) -> None:
     """Выполняет скрипт из файла для заполнения БД данными."""
-
+    f = open(script_file, 'r', encoding='utf-8')
+    queries = f.read()
+    cur.execute(queries)
 
 
 def create_suppliers_table(cur) -> None:
     """Создает таблицу suppliers."""
-    pass
+    query = '''CREATE TABLE suppliers (
+                supplier_id serial PRIMARY KEY,
+                company_name varchar,
+                contact varchar,
+                address varchar,
+                phone varchar,
+                fax varchar,
+                homepage varchar,
+                products varchar );'''
+    cur.execute(query)
 
 
 def get_suppliers_data(json_file: str) -> list[dict]:
     """Извлекает данные о поставщиках из JSON-файла и возвращает список словарей с соответствующей информацией."""
-    pass
+    with open(json_file, 'r', encoding='utf-8') as json_data:
+        data = json.load(json_data)
+        for i in data:
+            print(i)
+    return data
 
 
 def insert_suppliers_data(cur, suppliers: list[dict]) -> None:
     """Добавляет данные из suppliers в таблицу suppliers."""
-    pass
+    for i in suppliers:
+        values_data = ""
+        for j in list(i.values())[:-1]:
+            values_data = values_data + f"'{str(j).replace("'", "''")}',"
+        for j in i.get('products'):
+            query = (f"INSERT INTO suppliers ({','.join(list(i.keys()))})"
+                     f"VALUES ({values_data}'{j.replace("'", "''")}')")
+            cur.execute(query)
 
 
 def add_foreign_keys(cur, json_file) -> None:
